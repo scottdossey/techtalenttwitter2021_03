36fbcf69953980374b5f492b8982be84a390a1ce
Scott Dossey <sdossey@techtalentsouth.com>
2021-03-30 13:00:52 -0500

    Day 2's work. Add tweets, user page, page of users, following, and started adding Tags.

--- src/main/java/com/tts/TechTalentTwitter/model/User.java
+++ src/main/java/com/tts/TechTalentTwitter/model/User.java
@@ -1,6 +1,7 @@
 package com.tts.TechTalentTwitter.model;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -10,10 +11,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -32,15 +37,36 @@ public class User
     @Column(name="user_id")
     private Long id;
     
+    @Email(message = "Please provide a valid email")
+    @NotEmpty(message = "Please provide an email")
     private String email;
+    
+    @Length(min = 3, message = "Your username must have at least 3 characters")
+    @Length(max = 15, message = "Your username cannot have more than 15 characters")
+    @Pattern(regexp="[^\\s]+", message = "Your username cannot contain spaces")
     private String username;
+    
+    @Length(min = 5, message = "Your password must have at least 5 characters")
     private String password;
+    
+    @NotEmpty(message = "Please provide your first name")
     private String firstName;
+    
+    @NotEmpty(message = "Please provide your last name")
     private String lastName;
+    
     private int active;
     
     @CreationTimestamp
     private Date createdAt;
+    
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "user_follower", joinColumns = @JoinColumn(name="user_id"),
+        inverseJoinColumns = @JoinColumn(name = "follower_id"))            
+    private List<User> followers;
+    
+    @ManyToMany(mappedBy="followers")
+    private List<User> following;
     
     //Can't just store a set in a JPA entity---we are actually defining
     //a relationship between User and Role. This relationship will be
